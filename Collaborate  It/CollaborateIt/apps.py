fcccc4560da2c0f62a74fd9b0b0f372bf119691a
Nirmit Goyal <nirmitgoyal.goyal@gmail.com>
2017-02-20 21:35:36 +0530

    changed angular structure and added api's for compilation and running code for multiple languages

---
+++ Collaborate  It/CollaborateIt/apps.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+
+from django.apps import AppConfig
+
+
+class CollaborateItConfig(AppConfig):
+    name = 'CollaborateIt'
---
+++ Collaborate  It/CollaborateIt/models.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+
+from __future__ import unicode_literals
+
+from django.db import models
+from mongoengine import *
+
+# Create your models here.
